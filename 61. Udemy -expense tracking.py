beb139677a69fcf92147e80dfc1ca296593bf34b
christyfeifei <102733452+christyfeifei@users.noreply.github.com>
2022-04-22 10:56:52 +0800

    abridge 1st.

--- 61. Udemy -expense tracking.py
+++ 61. Udemy -expense tracking.py
@@ -5,8 +5,6 @@ while True:
     if name == 'q':
         break
     price = input('請輸入商品價格:')
-    p = []
-    p.append(name)
-    p.append(price)
+    p = [name, price]
     products.append(p)
 print(products)
